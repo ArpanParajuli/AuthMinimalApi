e1be4d0d5dc8729ba70018b16809091bc4db4f00
ArpanParajuli <arpanparajuli394@gmail.com>
2026-01-23 16:05:20 +0545

    Password test cases

--- MinimalApi.Tests/Domain/ValueObjects/PasswordTests.cs
+++ MinimalApi.Tests/Domain/ValueObjects/PasswordTests.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FluentAssertions;
+using MinimalApi.Domain.Exceptions;
+using MinimalApi.Domain.ValueObjects;
 
 namespace MinimalApi.Tests.Domain.ValueObjects
 {
-    internal class PasswordTests
+    public class PasswordTests
     {
+        [Theory]
+        [InlineData("arpandev132@")]
+        public void Password_InputValid_Returns_Value(string value)
+        {
+            // Act
+
+            var password = Password.Create(value);
+
+            // Assert
+
+            password.Value.Should().Be(value);
+            password.Value.Should().NotBeNullOrEmpty();
+            password.Value.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Password_Input_Null_Returns_Value(string value)
+        {
+            var PasswordExcepction = Assert.Throws<DomainException>(() =>
+            {
+                var password = Password.Create(value);
+            });
+
+            Assert.Equal("Password cannot be null or empty", PasswordExcepction.Message);
+        }
     }
 }
